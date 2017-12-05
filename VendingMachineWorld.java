caae8f5f467d8f714e8cc7772c2ce2efce2f3059
Brian Cheng <firius0408@gmail.com>
2017-12-04 20:39:51 -0800

    Created VendingMachineWorld and a few basic Actor subclasses needed. All empty

--- VendingMachineWorld.java
+++ VendingMachineWorld.java
@@ -1,21 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class VendingMachineWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class VendingMachineWorld extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class VendingMachineWorld.
      * 
      */
-    public MyWorld()
+    public VendingMachineWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(880, 880, 1); 
     }
 }
